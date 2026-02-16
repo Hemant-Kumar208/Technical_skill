26423cd980a0ec4c1dc48a7a49a9e8aa770f8003
Hemant <himanshuchaudhary5048@gmail.com>
2026-02-16 14:17:53 +0530

    Matrix questions

---
+++ src/rowSum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class rowSum {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row size: ");
+        int row = sc.nextInt();
+        System.out.println("Enter coloumn size: ");
+        int coloumn = sc.nextInt();
+
+        int[][] matrix = new int[row][coloumn];
+        System.out.println("Enter elements: ");
+        for(int i=0; i<row; i++){
+            for(int j=0; j < coloumn;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Array: ");
+        for(int i=0; i<row; i++) {
+            for (int j = 0; j < coloumn; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<row; i++) {
+            int sum = 0;
+            for(int j=0;j<row;j++){
+                sum = sum + matrix[i][j];
+            }
+            System.out.println("sum of row "+i+" is "+sum);
+        }
+    }
+}
